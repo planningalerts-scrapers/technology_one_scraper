4149ca6d0fce864c7f04ad95b4dba7a638c3a7e1
Matthew Landauer <mlandauer@gmail.com>
2019-06-25 17:11:33 +1000

    Small refactor

--- lib/technology_one_scraper/authority/fremantle.rb
+++ lib/technology_one_scraper/authority/fremantle.rb
@@ -4,67 +4,34 @@ require 'mechanize'
 module TechnologyOneScraper
   module Authority
     module Fremantle
-      def self.scrape_page(page)
+      def self.scrape_index_page(page)
         page.search("tr[@class='normalRow'], tr[@class='alternateRow']").each do |row|
           cells = row.search('td')
 
           council_reference = cells[0].search("a").text
-          if council_reference && council_reference.start_with?("DA", "LL", "VA", "WAPC", "ET", "PW") #selects planning applications only
-            puts "Found #{council_reference}"
-            save_record(council_reference, cells)
-          else
-            puts "skipping non planning application #{council_reference}"
-          end
+          yield(
+            'council_reference' => council_reference,
+            'address' => cells[5].search("a").text,
+            'description' => cells[2].text,
+            'info_url' => 'https://eservices.fremantle.wa.gov.au/ePropertyPROD/P1/eTrack/eTrackApplicationSearch.aspx?r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.ENQ',
+            'date_received' => Date.parse(cells[1].text).strftime("%Y-%m-%d"),
+            'date_scraped' => Date.today.to_s
+          )
         end
       end
 
-      def self.save_record(council_reference, cells)
-        record = {
-        'council_reference' => council_reference,
-        'address' => cells[5].search("a").text,
-        'description' => cells[2].text,
-        'info_url' => 'https://eservices.fremantle.wa.gov.au/ePropertyPROD/P1/eTrack/eTrackApplicationSearch.aspx?r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.ENQ',
-        'date_received' => Date.parse(cells[1].text).strftime("%Y-%m-%d"),
-        'date_scraped' => Date.today.to_s,
-        }
-        TechnologyOneScraper.save(record)
-      end
-
       def self.scrape_and_save
-        #scrape first results page
-        agent = Mechanize.new { |a|
-          #a.user_agent_alias = 'Mac Firefox'
-        }
+        agent = Mechanize.new
         url = "https://eservices.fremantle.wa.gov.au/ePropertyPROD/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=L7&r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.SL7"
         page = agent.get(url)
-        scrape_page(page)
 
-        # The above code searches the first 20 applications submitted in the last 7 days.
-        # There is sometimes more than 20 applications.
-        # The next 20 applicaitons are obtained by making a doPostform enquiry.
-        # I have drafted some code (below) which I have not been able to get to work.
-        # In the meantime, A small number of applicaitons may pass through the cracks. Most will be scraped.
-
-        #draft code:
-        #cp = 1
-        #np = $page.search("tr[@class='pagerRow'] td[@colspan]").to_s.split('"')[1].to_i
-        #if np == "" #if there is no second page
-          #np = 1
-        #end
-        #while cp > np
-          #cp+=1
-          #sleep 1.0 + rand
-          #form = $page.forms.first
-          #form["__EVENTTARGET"],form["__EVENTARGUMENT"] = "ctl00$Content$cusResultsGrid$repWebGrid$ctl00$grdWebGridTabularView", "page$#{cp}"
-          #$page = agent.submit(form)
-          #puts "scraping page number " + cp.to_s
-          #scrape_page
-        #end
-
-        #look at these pages for tips:
-          #https://blog.scraperwiki.com/2011/11/how-to-get-along-with-an-asp-webpage/
-          #http://scraperblog.blogspot.com.au/2012/10/asp-forms-with-dopostback-using-ruby.html
-          #http://mechanize.rubyforge.org/Mechanize/Form.html
+        # TODO: Add pagination support
+        scrape_index_page(page) do |record|
+          # TODO: Make the search ignore these rather than filtering them out here
+          if record["council_reference"].start_with?("DA", "LL", "VA", "WAPC", "ET", "PW") #selects planning applications only
+            TechnologyOneScraper.save(record)
+          end
+        end
       end
     end
   end
