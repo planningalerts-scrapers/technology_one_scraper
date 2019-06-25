c79c441d4d04acc93f30cdb0a8512d31a49226ca
Matthew Landauer <mlandauer@gmail.com>
2019-06-25 16:18:40 +1000

    Make info_url from the current url

--- lib/technology_one_scraper/authority/cockburn.rb
+++ lib/technology_one_scraper/authority/cockburn.rb
@@ -6,14 +6,19 @@ require 'mechanize'
 module TechnologyOneScraper
   module Authority
     module Cockburn
-      def self.scrape_and_save_index_page(page, info_url)
+      def self.scrape_and_save_index_page(page)
         results = page.search("tr.normalRow, tr.alternateRow")
+
         results.each do |result|
+          council_reference = result.search("td")[0].inner_text
+          info_url = 'eTrackApplicationDetails.aspx?r=P1.WEBGUEST&f=%24P1.ETR.APPDET.VIW&ApplicationId=' +
+                     # TODO: Do proper escaping rather than this hack
+                     council_reference.gsub("/", "%2f")
           record = {
-            'council_reference' => result.search("td")[0].inner_text.to_s,
+            'council_reference' => council_reference,
             'address'           => result.search("td")[3].inner_text.to_s,
             'description'       => result.search("td")[2].inner_text.to_s,
-            'info_url'          => info_url + result.search("td")[0].inner_text.sub!("/", "%2f"),
+            'info_url'          => (page.uri + info_url).to_s,
             'date_scraped'      => Date.today.to_s,
             'date_received'     => Date.parse(result.search("td")[1]).to_s
           }
@@ -49,7 +54,6 @@ module TechnologyOneScraper
         period = "TM"
 
         url         = 'https://ecouncil.cockburn.wa.gov.au/eProperty/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=' + period +'&r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.S' + period
-        info_url    = 'https://ecouncil.cockburn.wa.gov.au/eProperty/P1/eTrack/eTrackApplicationDetails.aspx?r=P1.WEBGUEST&f=%24P1.ETR.APPDET.VIW&ApplicationId='
 
         agent = Mechanize.new
         page = agent.get(url)
@@ -57,7 +61,7 @@ module TechnologyOneScraper
         i = 1
         while page
           puts "Scraping page #{i}..."
-          scrape_and_save_index_page(page, info_url)
+          scrape_and_save_index_page(page)
           page = next_page(page, i)
           i += 1
         end
