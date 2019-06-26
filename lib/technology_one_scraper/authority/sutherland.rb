664ee6c0ac0299aa88743c8f32ed1c24da0aba70
Matthew Landauer <mlandauer@gmail.com>
2019-06-27 01:51:22 +1000

    Use library

--- lib/technology_one_scraper/authority/sutherland.rb
+++ lib/technology_one_scraper/authority/sutherland.rb
@@ -20,22 +20,6 @@ module TechnologyOneScraper
         end
       end
 
-      # Implement a click on a link that understands stupid asp.net doPostBack
-      def self.click(page, link)
-        href = link["href"]
-        if href =~ /javascript:__doPostBack\(\'(.*)\',\'(.*)'\)/
-          event_target = $1
-          event_argument = $2
-          form = page.form_with(id: "aspnetForm")
-          form["__EVENTTARGET"] = event_target
-          form["__EVENTARGUMENT"] = event_argument
-          form.submit
-        else
-          # TODO Just follow the link likes it's a normal link
-          raise
-        end
-      end
-
       def self.scrape_and_save
         period = 'TM'
 
@@ -60,7 +44,7 @@ module TechnologyOneScraper
             if next_page_link
               current_page_no += 1
               puts "Getting page #{current_page_no}..."
-              page = click(page, next_page_link)
+              page = Postback.click(next_page_link, page)
             end
           end
         end
