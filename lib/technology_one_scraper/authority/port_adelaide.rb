dac777aba31981dd61588c08da4af1af6ca00fd8
Matthew Landauer <mlandauer@gmail.com>
2019-06-26 15:38:25 +1000

    Extract and inline method

--- lib/technology_one_scraper/authority/port_adelaide.rb
+++ lib/technology_one_scraper/authority/port_adelaide.rb
@@ -5,23 +5,21 @@ require 'uri'
 module TechnologyOneScraper
   module Authority
     module PortAdelaide
-      def self.scrape_and_save_page(page)
+      def self.scrape_page(page)
         page.search("tr[@class='normalRow'], tr[@class='alternateRow']").each do |row|
           cells = row.search('td')
 
           council_reference = cells[0].search("a").text
           if council_reference && council_reference.start_with?("040") #selects planning applications only
             puts "Found #{council_reference}"
-            record = {
+            yield(
               'council_reference' => council_reference,
               'address' => cells[3].text,
               'description' => cells[2].text,
               'info_url' => "https://ecouncil.portenf.sa.gov.au/T1PRWebPROD/eProperty/P1/eTrack/eTrackApplicationDetails.aspx?r=PAE.P1.WEBGUEST&f=%24P1.ETR.APPDET.VIW&ApplicationId=#{URI::escape(council_reference)}",
               'date_received' => Date.parse(cells[1].text).strftime("%Y-%m-%d"),
-              'date_scraped' => Date.today.to_s,
-            }
-
-            TechnologyOneScraper.save(record)
+              'date_scraped' => Date.today.to_s
+            )
           else
             puts "skipping non planning application #{council_reference}"
           end
@@ -33,7 +31,9 @@ module TechnologyOneScraper
         url = "https://ecouncil.portenf.sa.gov.au/T1PRWebPROD/eProperty/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=L7&r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.SL7"
         page = agent.get(url)
         # TODO: Handle pagination
-        scrape_and_save_page(page)
+        scrape_page(page) do |record|
+          TechnologyOneScraper.save(record)
+        end
       end
     end
   end
