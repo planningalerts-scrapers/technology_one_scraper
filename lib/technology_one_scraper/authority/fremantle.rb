1286cb3d4f9ecb015d566323dfe775d8a3fef2be
Matthew Landauer <mlandauer@gmail.com>
2019-06-26 12:34:04 +1000

    Use library

--- lib/technology_one_scraper/authority/fremantle.rb
+++ lib/technology_one_scraper/authority/fremantle.rb
@@ -5,22 +5,14 @@ module TechnologyOneScraper
   module Authority
     module Fremantle
       def self.scrape_and_save
-        url = TechnologyOneScraper.url_period(
+        TechnologyOneScraper.scrape_period(
           "https://eservices.fremantle.wa.gov.au/ePropertyPROD",
           "L28"
-        )
-
-        agent = Mechanize.new
-        page = agent.get(url)
-
-        while page
-          Page::Index.scrape(page) do |record|
-            # TODO: Make the search ignore these rather than filtering them out here
-            if record["council_reference"].start_with?("DA", "LL", "VA", "WAPC", "ET", "PW") #selects planning applications only
-              TechnologyOneScraper.save(record)
-            end
+        ) do |record|
+          # TODO: Make the search ignore these rather than filtering them out here
+          if record["council_reference"].start_with?("DA", "LL", "VA", "WAPC", "ET", "PW") #selects planning applications only
+            TechnologyOneScraper.save(record)
           end
-          page = Page::Index.next(page)
         end
       end
     end
