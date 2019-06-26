c37d712480be2dffd6b40e8e309d98dea5dbc6e0
Matthew Landauer <mlandauer@gmail.com>
2019-06-26 15:20:49 +1000

    Generalise a little

--- lib/technology_one_scraper/authority/noosa.rb
+++ lib/technology_one_scraper/authority/noosa.rb
@@ -8,26 +8,33 @@ module TechnologyOneScraper
       def self.scrape_and_save
         period = "TM"
 
-        url         = 'https://noo-web.t1cloud.com/T1PRDefault/WebApps/eProperty/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=' + period +'&r=P1.WEBGUEST&f=$P1.ETR.SEARCH.S' + period
-        info_url    = 'https://noo-web.t1cloud.com/T1PRDefault/WebApps/eProperty/P1/eTrack/eTrackApplicationDetails.aspx?r=P1.WEBGUEST&f=$P1.ETR.APPDET.VIW&ApplicationId='
+        url = 'https://noo-web.t1cloud.com/T1PRDefault/WebApps/eProperty/P1/eTrack/eTrackApplicationSearchResults.aspx?Field=S&Period=' + period +'&r=P1.WEBGUEST&f=$P1.ETR.SEARCH.S' + period
 
         agent = Mechanize.new
         agent_detail_page = Mechanize.new
         page = agent.get(url)
 
         while page
-          Page::Index.scrape(page) do |record_index|
-            detail_page = agent_detail_page.get(record_index[:info_url])
-            record_detail = Page::Detail.scrape(detail_page)
-            record = {
-              'council_reference' => record_index[:council_reference],
-              'address' => record_detail[:address],
-              'description' => record_detail[:description],
-              'info_url' => record_index[:info_url],
+          Page::Index.scrape(page) do |record|
+            if record[:council_reference].nil? ||
+               record[:address].nil? ||
+               record[:description].nil? ||
+               record[:date_received].nil?
+              # We need more information. We can get this from the detail page
+              detail_page = agent_detail_page.get(record[:info_url])
+              record_detail = Page::Detail.scrape(detail_page)
+              record = record.merge(record_detail)
+              # TODO: Check that we have enough now
+            end
+
+            TechnologyOneScraper.save(
+              'council_reference' => record[:council_reference],
+              'address' => record[:address],
+              'description' => record[:description],
+              'info_url' => record[:info_url],
               'date_scraped' => Date.today.to_s,
-              'date_received' => record_index[:date_received]
-            }
-            TechnologyOneScraper.save(record)
+              'date_received' => record[:date_received]
+            )
           end
           page = Page::Index.next(page)
         end
