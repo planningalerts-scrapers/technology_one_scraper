dcdd6ffb2823ec327871d754f5fc5213cabd114a
Matthew Landauer <mlandauer@gmail.com>
2019-06-26 15:45:03 +1000

    Extract method

--- lib/technology_one_scraper/authority/port_adelaide.rb
+++ lib/technology_one_scraper/authority/port_adelaide.rb
@@ -5,23 +5,33 @@ require 'uri'
 module TechnologyOneScraper
   module Authority
     module PortAdelaide
-      def self.scrape_page(page)
+      def self.scrape_page2(page)
         page.search("tr[@class='normalRow'], tr[@class='alternateRow']").each do |row|
           cells = row.search('td')
 
           council_reference = cells[0].search("a").text
-          if council_reference && council_reference.start_with?("040") #selects planning applications only
-            puts "Found #{council_reference}"
+          yield(
+            council_reference: council_reference,
+            address: cells[3].text,
+            description: cells[2].text,
+            info_url: "https://ecouncil.portenf.sa.gov.au/T1PRWebPROD/eProperty/P1/eTrack/eTrackApplicationDetails.aspx?r=PAE.P1.WEBGUEST&f=%24P1.ETR.APPDET.VIW&ApplicationId=#{URI::escape(council_reference)}",
+            date_received: Date.parse(cells[1].text).strftime("%Y-%m-%d")
+          )
+        end
+      end
+
+      def self.scrape_page(page)
+        scrape_page2(page) do |record|
+          # selects planning applications only
+          if record[:council_reference] && record[:council_reference].start_with?("040")
             yield(
-              'council_reference' => council_reference,
-              'address' => cells[3].text,
-              'description' => cells[2].text,
-              'info_url' => "https://ecouncil.portenf.sa.gov.au/T1PRWebPROD/eProperty/P1/eTrack/eTrackApplicationDetails.aspx?r=PAE.P1.WEBGUEST&f=%24P1.ETR.APPDET.VIW&ApplicationId=#{URI::escape(council_reference)}",
-              'date_received' => Date.parse(cells[1].text).strftime("%Y-%m-%d"),
+              'council_reference' => record[:council_reference],
+              'address' => record[:address],
+              'description' => record[:description],
+              'info_url' => record[:info_url],
+              'date_received' => record[:date_received],
               'date_scraped' => Date.today.to_s
             )
-          else
-            puts "skipping non planning application #{council_reference}"
           end
         end
       end
