acd76ad3e0f1908180234ada29a0ce0ce61448a3
Matthew Landauer <mlandauer@gmail.com>
2019-06-26 12:04:01 +1000

    Extract method

--- lib/technology_one_scraper/authority/manningham.rb
+++ lib/technology_one_scraper/authority/manningham.rb
@@ -10,6 +10,22 @@ module TechnologyOneScraper
         form.submit
       end
 
+      def self.scrape_and_save_index_page(page, info_url)
+        results = page.search("tr.normalRow, tr.alternateRow")
+        results.each do |result|
+          record = {
+            'council_reference' => result.search("td")[0].inner_text.to_s,
+            'address'           => result.search("td")[3].inner_text.to_s,
+            'description'       => result.search("td")[2].inner_text.to_s,
+            'info_url'          => info_url + result.search("td")[0].inner_text.sub!("/", "%2f"),
+            'date_scraped'      => Date.today.to_s,
+            'date_received'     => Date.parse(result.search("td")[1]).to_s
+          }
+
+          TechnologyOneScraper.save(record)
+        end
+      end
+
       def self.scrape_and_save
         period = "TM"
 
@@ -36,19 +52,7 @@ module TechnologyOneScraper
             page = postback(page.form, target, 'Page$' + i.to_s)
           end
 
-          results = page.search("tr.normalRow, tr.alternateRow")
-          results.each do |result|
-            record = {
-              'council_reference' => result.search("td")[0].inner_text.to_s,
-              'address'           => result.search("td")[3].inner_text.to_s,
-              'description'       => result.search("td")[2].inner_text.to_s,
-              'info_url'          => info_url + result.search("td")[0].inner_text.sub!("/", "%2f"),
-              'date_scraped'      => Date.today.to_s,
-              'date_received'     => Date.parse(result.search("td")[1]).to_s
-            }
-
-            TechnologyOneScraper.save(record)
-          end
+          scrape_and_save_index_page(page, info_url)
         end
       end
     end
